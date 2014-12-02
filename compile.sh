a87a07d92fa3d4adae3ec187d59330419eb939c5
Nikolay Malkov <Nikolay.Malkov@portaone.com>
2014-12-02 11:20:24 +0200

    Fix patterns

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@ arm-none-eabi-as -o arm.o arm.s
 arm-none-eabi-ld -T arm.lds -o arm.elf arm.o
 arm-none-eabi-objcopy -O binary arm.elf arm.bin
 dd if=arm.bin of=flash.bin bs=4096 conv=notrunc
-qemu-system-arm -M connex -m 8k -pflash flash.bin -nographic -serial stdio
+qemu-system-arm -M connex -pflash flash.bin -nographic -serial /dev/null
